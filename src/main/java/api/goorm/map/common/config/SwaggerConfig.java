e7a628da5d4cbd55d23399c44121950a58100719
SongJaeHoonn <thdwogns121@naver.com>
2024-09-22 20:15:16 +0900

    feat: 시큐리티, 스웨거 설정

--- src/main/java/api/goorm/map/common/config/SwaggerConfig.java
+++ src/main/java/api/goorm/map/common/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package api.goorm.map.common.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,8 +14,16 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+                .components(new Components().addSecuritySchemes("cookieAuth", cookieSecurityScheme()))
+                .info(apiInfo())
+                .addSecurityItem(new SecurityRequirement().addList("cookieAuth"));
+    }
+
+    private SecurityScheme cookieSecurityScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.COOKIE)
+                .name("AccessToken");
     }
 
     private Info apiInfo() {
