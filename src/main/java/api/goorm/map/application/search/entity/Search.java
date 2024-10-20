914defd7dbf6ef840874176beef963c3489f612f
SongJaeHoonn <thdwogns121@naver.com>
2024-10-20 21:24:13 +0900

    refactor: Location까지 삭제되지 않도록 변경

--- src/main/java/api/goorm/map/application/search/entity/Search.java
+++ src/main/java/api/goorm/map/application/search/entity/Search.java
@@ -22,7 +22,7 @@ public class Search {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "location_id")
     private Location location;
 
